80e91bdc70648fc74a11d03d84c064e9be25ec1e
Vexaro Studios <cloudifylauncher@gmail.com>
2025-11-22 21:27:16 +0100

    Updated script.js

--- script.js
+++ script.js
@@ -1,10 +1,15 @@
-// Activate icons
+// --- SIDEBAR TOGGLE ---
+function toggleSidebar() {
+    document.getElementById("sidebar").classList.toggle("active");
+}
+
+// --- ACTIVATE LUCIDE ICONS ---
 lucide.createIcons();
 
-// Load particles
+// --- LOAD PARTICLES ---
 particlesJS.load("particles-js", "particles.json");
 
-// INTRO REMOVE AFTER ANIMATION
+// --- INTRO REMOVE AFTER ANIMATION ---
 setTimeout(() => {
     document.getElementById("intro").style.display = "none";
 }, 2400);
